bf9d82a6736351b371520febdf311657d64e1042
Takeshi Fujino <fujino@edu.k.u-tokyo.ac.jp>
2023-08-02 16:15:24 +0900

    FEAT: download per directory to fetch index files

--- create_input_data_set.rb
+++ create_input_data_set.rb
@@ -17,6 +17,7 @@ input = WorkflowInputParser.run(workflow_input_json_path)
 workspace_data = TableParser.run(workspace_data_tsv_path)
 sample = TableParser.run(sample_tsv_path)
 
+copy_table = {}
 input.transform_values! do |v|
   next v unless v.is_a?(DataPointer)
 
@@ -35,16 +36,23 @@ input.transform_values! do |v|
   next v0 unless v0.is_a?(String)
   next v0 unless v0 =~ %r{^gs://(.+)$}
 
-  dst_path = download_dir / Regexp.last_match(1)
-  FileUtils.mkpath(dst_path.dirname)
+  src_path = Pathname.new(Regexp.last_match(1))
+  src_dir = src_path.dirname
+  dst_path = download_dir / src_path
+  dst_dir = dst_path.dirname
+  FileUtils.mkpath(dst_dir)
+  copy_table["gs://#{src_dir}"] = dst_dir
 
-  warn "Downloading #{v0}"
-  Open3.popen3("gsutil cp #{v0} #{dst_path.dirname}") do |_, o, e, _|
-    o.each { |line| warn line }
-    e.each { |line| warn line }
-  end
   dst_path.to_s
 end
 input.compact!
+
+copy_table.each do |src_dir_uri, dst_dir|
+  warn "Downloading #{src_dir_uri}"
+  Open3.popen3("gsutil cp -r #{src_dir_uri} #{dst_dir.dirname}") do |_, o, e, _|
+    o.each { |line| warn line }
+    e.each { |line| warn line }
+  end
+end
 
 puts JSON.generate(input)
